ddb5941efee45dd244d0efb2ffda526a33aa8b96
DaniilMarinovich <87134258+Domanstik@users.noreply.github.com>
2024-12-25 18:59:21 +0300

    Add base PersonInfo entity in Domain

---
+++ DataInOrder.Domain/Entities/PersonInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace DataInOrder.Domain.Entities;
+
+public class PersonInfo(DateTime date, string firstName, string lastName, string surName, string city, string country)
+{
+    private readonly Guid Id = Guid.NewGuid();
+    public DateTime Date { get; set; } = date;
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public string SurName { get; set; } = surName;
+    public string City { get; set; } = city;
+    public string Country { get; set; } = country;
+}
