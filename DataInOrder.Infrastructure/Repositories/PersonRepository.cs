b6a2abf3dbe3114376135522895960caf4b69828
DaniilMarinovich <marinovich.daniil@gmail.com>
2025-01-01 22:12:52 +0300

    Added filters in repositoty query

--- DataInOrder.Infrastructure/Repositories/PersonRepository.cs
+++ DataInOrder.Infrastructure/Repositories/PersonRepository.cs
@@ -6,16 +6,108 @@ namespace DataInOrder.Infrastructure.Repositories
 {
     public class PersonRepository: IPesonRepository
     {
-        readonly PersonContext  _context;
+        readonly PersonContext _context;
+        private Dictionary<string, string> filters;
+        private IQueryable<Person> query;
 
-        public Task CreatePersonAsunc(Person personInfo)
+        PersonRepository(PersonContext context)
         {
-            return Task.CompletedTask;
+            _context = context;
+            filters = [];
+            query = context.Persons;
         }
 
-        public Task<Person> GetById(Guid id)
+        public void AddFilter(string filter)
         {
-            throw new NotImplementedException();
+            filters.Add(filter, string.Empty);
+        }
+
+        public void AddFilter(string filter, string value)
+        {
+            filters.Add(filter, value);
+        }
+
+        public void RemoveFilter(string filter)
+        {
+            filters.Remove(filter);
+        }
+
+        private IQueryable<Person> ApplyFilters()
+        {
+            ClearQuery();
+
+            foreach (var filter in filters.Keys)
+            {
+                switch (filter)
+                {
+                    case "DateAsc":
+                        query = query.OrderBy(p => p.Date);
+                        break;
+                    case "FirstNameAsc":
+                        query = query.OrderBy(p => p.FirstName);
+                        break;
+                    case "LastNameAsc":
+                        query = query.OrderBy(p => p.LastName);
+                        break;
+                    case "SurNameAsc":
+                        query = query.OrderBy(p => p.SurName);
+                        break;
+                    case "CityAsc":
+                        query = query.OrderBy(p => p.City);
+                        break;
+                    case "CountryAsc":
+                        query = query.OrderBy(p => p.Country);
+                        break;
+                    case "DateDesc":
+                        query = query.OrderByDescending(p => p.Date);
+                        break;
+                    case "FirstNameDesc":
+                        query = query.OrderByDescending(p => p.FirstName);
+                        break;
+                    case "LastNameDesc":
+                        query = query.OrderByDescending(p => p.LastName);
+                        break;
+                    case "SurNameDesc":
+                        query = query.OrderByDescending(p => p.SurName);
+                        break;
+                    case "CityDesc":
+                        query = query.OrderByDescending(p => p.City);
+                        break;
+                    case "CountryDesc":
+                        query = query.OrderByDescending(p => p.Country);
+                        break;
+                    case "DateSearch":
+                        query = query.Where(p => p.Date == DateTime.Parse(filters[filter]));
+                        break;
+                    case "FirstNameSearch":
+                        query = query.Where(p => p.FirstName == filters[filter]);
+                        break;
+                    case "LastNameSearch":
+                        query = query.Where(p => p.LastName == filters[filter]);
+                        break;
+                    case "SurNameSearch":
+                        query = query.Where(p => p.SurName == filters[filter]);
+                        break;
+                    case "CitySearch":
+                        query = query.Where(p => p.City == filters[filter]);
+                        break;
+                    case "CountrySearch":
+                        query = query.Where(p => p.Country == filters[filter]);
+                        break;
+                }
+            }
+
+            return query.AsQueryable();
+        }
+
+        private void ClearQuery()
+        {
+            query = _context.Persons;
+        }
+
+        public List<Person> GetFilteredPersons()
+        {
+            return [.. ApplyFilters()];
         }
     }
 }
