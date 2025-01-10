98605b50c6bc71dcee87bb67aec4417574ff0b0c
DaniilMarinovich <marinovich.daniil@gmail.com>
2025-01-10 13:31:26 +0300

    Added converter from data to xml

---
+++ DataInOrder.Application/Services/ConvertToXML.cs
@@ -0,0 +1,33 @@
+﻿using DataInOrder.Application.Interfaces;
+using DataInOrder.Domain.Entities;
+using System.Xml.Linq;
+
+
+namespace DataInOrder.Application.Services;
+
+public class ConvertToXML : IConvert
+{
+    public void Convert(List<Person> persons, string fullPath)
+    {
+        XDocument xdoc = new XDocument();
+        XElement personsXML = new XElement("Persons");
+
+    
+        foreach (Person person in persons)
+        {
+            XElement tempPerson = new XElement("Person",
+                new XAttribute("id", person.Id),
+                new XElement("Date", person.Date),
+                new XElement("FirstName", person.FirstName),
+                new XElement("LastName", person.LastName),
+                new XElement("SurName", person.SurName),
+                new XElement("City", person.City),
+                new XElement("Country", person.Country));
+
+            personsXML.Add(tempPerson);
+        }
+
+        xdoc.Add(personsXML);
+        xdoc.Save(fullPath);
+    }
+}
